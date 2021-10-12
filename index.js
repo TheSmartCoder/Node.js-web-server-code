0dc1bd6049b5cb452f2c4f781a8d645e2546da91
TheSmartCoder <92354577+TheSmartCoder@users.noreply.github.com>
2021-10-12 18:07:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('HELLO WORLD'));
+
+app.listen(port, () => console.log('Your Project is listening to http://localhost:${port}'));
+                                   
